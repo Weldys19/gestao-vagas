aa8ef6fe1e1b8ce66820878dc3d107df28d96808
Weldys Andrade do Carmo <weldyscarmo27@gmail.com>
2025-12-29 12:31:58 -0300

    Criando estrutura do projeto|Inserindo validação|Formatando mensagem de validação|Inserindo Spring Data

---
+++ src/main/java/com/br/weldyscarmo/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,26 @@
+package com.br.weldyscarmo.gestao_vagas.modules.candidate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um email válido")
+    private String email;
+
+    @Length(min = 10, max = 100, message = "A senha deve conter entre 10 e 100 caracters")
+    private String password;
+    private String description;
+    private String curriculum;
+}
